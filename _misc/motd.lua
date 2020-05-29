212bd700408812f28f745f29f28b9dffaabb6f2d
WilLiam12 <65061739+WilLiam12-teddy@users.noreply.github.com>
2020-05-29 20:36:23 -0300

    .

--- _misc/motd.lua
+++
@@ -1,4 +0,0 @@
-minetest.register_on_joinplayer(function(player)
-	local message = "# Server: Skins & Sethome have recently been removed from the inventory. Use the commands /skin, /sethome and /home."
-	minetest.chat_send_player(player:get_player_name(), message)
-end)
