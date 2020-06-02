1af4cd2fe0104074a79e927bed92e184112a2201
WilLiam12 <65061739+WilLiam12-teddy@users.noreply.github.com>
2020-06-02 06:02:00 -0300

    .

---
+++ ufos/ufos/config.lua
@@ -0,0 +1,20 @@
+modUFO = {
+	fuel_type = "ufos:ionized_bioresin",
+	ship_acceleration = 0.8,
+	ship_brake = 0.5,
+	ship_turn_speed = 1.0, --2.0
+	ship_max_speed = 20,
+	ship_fuel_use = 0.005, --percente per step
+	forge = {
+		fuel_use = 0.100, --percente per second
+		sizeSrc ={ 
+			width = 3,
+			height = 1,
+		},
+		sizeDst ={
+			width = 3,
+			height = 2,
+		},
+	},
+}
+
