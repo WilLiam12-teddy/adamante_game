8e6d949fa9e876ee4b10d2ced8599263b0ade221
WilLiam12 <65061739+WilLiam12-teddy@users.noreply.github.com>
2020-06-04 01:22:46 -0300

    .

---
+++ repairer/config.lua
@@ -0,0 +1,9 @@
+repairer = {
+	urlTabela = minetest.get_worldpath().."/repairer.tbl",
+	isPrintInPublicChat = (minetest.setting_get("repairer.printInPublicChat")~="false"),
+	replace = {},
+	nodes = {},
+	replace = {
+		nodes = {},
+	},
+}
