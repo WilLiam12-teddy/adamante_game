f1a99cb6e8fbcba37cc053946e74dcdc74376cd8
WilLiam12-teddy <willteddydoze@gmail.com>
2020-06-11 11:51:48 -0300

    .

--- tradelands/translate.lua
+++
@@ -1,8 +0,0 @@
---S = rawget(_G, "intllib") and intllib.Getter() or function(s) return s end
-modTradeLands.translate = rawget(_G, "intllib") and intllib.Getter() or function(s) return s end
-
---[[
-S("%s moves stuff in %s at %s"):format(
-	player:get_player_name(), name, minetest.pos_to_string(pos)
-)
---]]
