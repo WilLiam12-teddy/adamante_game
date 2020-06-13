367703ce890e6524af84db3714d954711ecb409a
WilLiam12-teddy <willteddydoze@gmail.com>
2020-06-12 22:46:48 -0300

    .

--- outdoor/init.lua
+++ outdoor/init.lua
@@ -22,14 +22,26 @@ outdoor.register_simbol("simbol_danger_radioactive", "Perigo: Radioatividade", "
 outdoor.register_simbol("simbol_danger_monsters", "Perigo: Monstros", "simbol_danger_monsters.png", "PERIGO: MONSTROS")
 outdoor.register_simbol("simbol_danger_monsters2", "Perigo: Fantasmas", "simbol_danger_monsters2.png", "PERIGO: FANTASMAS")
 outdoor.register_simbol("sign_dontfeedthetrolls", "Proibido Alimentar Trolls", "sign_dontfeedthetrolls.png", "PROIBIDO ALIMENTAR TROLLS")
+outdoor.register_simbol("sign_constrution_zone_256", "Placa 'Area em Construcao'", "sign_constrution_zone_256x256.png")
+outdoor.register_simbol("sign_elec_shock_risk", "Risco de Choque Elétrico", "sign_electrical-shock-risk_96x96.png", "RISCO DE CHOQUE ELÉTRICO")
+outdoor.register_simbol("sign_falling_risk", "Risco de Queda", "sign_falling_risk_256x256.png")
+outdoor.register_simbol("sign_nao_vendo_fiado", "Não Vendo Fiado", "sign_nao_vendo_fiado_256x256.png")
+outdoor.register_simbol("sign_nao_sinta_inveja", "Não Sinta Inveja", "sign_nao_sinta_inveja_256x256.png")
+outdoor.register_simbol("sign_inveja_do_gato", "CUIDADO: Inveja do Gato", "sign_inveja_do_gato_256x256.png")
+outdoor.register_simbol("sign_comer_coco", "Proibido Comer Coco", "sign_proibido_comer_coco_149x151.png")
+outdoor.register_simbol("sign_dona_brava", "CUIDADO: Dona Brava", "sign_cuidado_dona_brava_195x195.png")
+outdoor.register_simbol("sign_sua_higiene", "CUIDADO: Com Sua Higiêne", "sign_sua_higiene_512x512.png")
 
 outdoor.register_simbol("sign_hammer", "Loja de Ferramentas", "sign_hammer.png", "Loja de Ferramentas")
 outdoor.register_simbol("sign_hospital", "Hospital/Enfermaria", "sign_hospital.png")
 outdoor.register_simbol("sign_bank", "Banco (1)", "sign_bank.png", "Banco")
 outdoor.register_simbol("sign_bank2", "Banco (2)", "sign_bank2.png", "Banco")
 outdoor.register_simbol("sign_hotel", "Hospedaria", "sign_hotel.png", "Hospedaria")
+outdoor.register_simbol("simbol_distintivo", "Delegacia de Policia", "simbol_distintivo.png", "Delegacia de Policia")
+outdoor.register_simbol("simbol_sheriff", "Xerife", "simbol_sheriff.png")
 outdoor.register_simbol("sign_open", "Placa 'Aberto'", "sign_open.png", "Placa 'Aberto'")
 outdoor.register_simbol("sign_close", "Placa 'Fechado'", "sign_close.png", "Placa 'Fechado'")
+
 outdoor.register_simbol("sign_alugase1", "Placa 'Aluga-se (Terreno)'", "sign_aluga-se1.png")
 outdoor.register_simbol("sign_alugase2", "Placa 'Aluga-se (Generico)'", "sign_aluga-se2.png")
 outdoor.register_simbol("sign_vendese1", "Placa 'Vende-se (Terreno)'", "sign_vende-se1.png")
@@ -75,11 +87,7 @@ outdoor.register_simbol("frame_apocalipse", "Quadro 'Apocalipse'", "frame.png^fr
 outdoor.register_simbol("frame_inteligencia", "Quadro 'Inteligencia'", "frame.png^frame_inteligencia.png", "Quadro 'Inteligencia'")
 outdoor.register_simbol("frame_perseveranca", "Quadro 'Perseveranca'", "frame.png^frame_perseveranca.png", "Quadro 'Perseveranca'")
 outdoor.register_simbol("frame_preconceito", "Quadro 'Preconceito'", "frame.png^frame_preconceito.png", "Quadro 'Preconceito'")
-
---outdoor.register_simbol("photo_lucas", "Foto de Lucas (UNDERTAKER)", "photo_lucas.png", "Foto de Lucas (UNDERTAKER)")
---outdoor.register_simbol("photo_adonay", "Foto de Adonay (ADFENO)", "photo_adonay.png", "Foto de Adonay (ADFENO)")
---outdoor.register_simbol("simbol_distintivo", "Delegacia de Policia", "simbol_distintivo.png", "Delegacia de Policia")
---outdoor.register_simbol("simbol_sheriff", "Xerife", "simbol_sheriff.png")
+outdoor.register_simbol("frame_craftopoles", "Quadro 'Craftopoles' (2020-06-12)", "frame.png^frame_craftopoles_2020-06-12.png", "Quadro 'Craftopoles' (2020-06-12)")
 
 --#############################################################################################################
 
