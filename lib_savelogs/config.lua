d4fcecbc4c52311220133ea96fa26a56dec4af51
WilLiam12-teddy <willteddydoze@gmail.com>
2020-06-17 17:32:46 -0300

    .

---
+++ lib_savelogs/config.lua
@@ -0,0 +1,5 @@
+libSaveLogs = { 
+	savePath = minetest.get_worldpath(), --Pasta onde serão salvas os logs. (sem usar barra ['/' ou '\'] no final!!!)
+	saveInterval = 60, --Salva a casa 60 segundos
+	savePosOfSpeaker = true, --Se vai salvar as posições de quem falar algo.
+}
