e5697f80b2fe9ace0d82059f411bff600179550b
WilLiam12-teddy <willteddydoze@gmail.com>
2020-06-23 15:58:52 -0300

    .

---
+++ cidade/README.md
@@ -0,0 +1,3 @@
+--Cidade Minetest Mod.
+
+this mod add some itens for my city.
---
+++ cidade/init.lua
@@ -0,0 +1,187 @@
+minetest.register_node("cidade:nyan", {
+    description = "Nyan Cat Azul",
+    tiles = {"nyan_cat_blue.png"},
+    is_ground_content = true,
+    groups = {cracky=3, stone=1}
+})
+
+minetest.register_node("cidade:apartment", {
+    description = "Apartamento",
+    tiles = {"apartment_controls_vacant.png"},
+    is_ground_content = true,
+    groups = {cracky=3, stone=1}
+})
+
+local pictures = {   ["manlec"]="Logo Manlec",
+}
+
+for name,desc in pairs(pictures) do
+    minetest.register_node("cidade:"..name, {
+        description = desc,
+        drawtype = "signlike",
+        inventory_image = name..".png",
+        wield_image = name..".png",
+        visual_scale = 5.0,
+        tiles = {name..".png",},
+        use_texture_alpha = true,
+        paramtype = "light",
+        paramtype2 = "wallmounted",
+        sunlight_propagates = true,
+        walkable = false,
+        light_source = 5,
+        is_ground_content = false,
+        selection_box = {
+            type = "wallmounted",
+            fixed = {-0.5, -0.5, -0.5, 0.5, -0.4, 0.5}
+        },
+        groups = {cracky=3,dig_immediate=3},
+        on_construct = function(pos)
+            local msg = desc
+            local meta = minetest.get_meta(pos)
+            --meta:set_string("text", msg)
+            meta:set_string("infotext", '"' .. msg .. '"')
+        end,
+    })
+end
+
+minetest.register_node("cidade:manlec_block", {
+    description = "Bloco de Parede para Manlec",
+    tiles = {"manlec_paredes.png"},
+    is_ground_content = true,
+    groups = {cracky=3, stone=1}
+})
+
+minetest.register_node("cidade:nacional_marrom", {
+    description = "Bloco de Baixo do Nacional",
+    tiles = {"nacional_marrom.png"},
+    is_ground_content = true,
+    groups = {cracky=3, stone=1}
+})
+
+local pictures = {   ["nacional"]="Nacional",
+}
+
+for name,desc in pairs(pictures) do
+    minetest.register_node("cidade:"..name, {
+        description = desc,
+        drawtype = "signlike",
+        inventory_image = name..".png",
+        wield_image = name..".png",
+        visual_scale = 7.0,
+        tiles = {name..".png",},
+        use_texture_alpha = true,
+        paramtype = "light",
+        paramtype2 = "wallmounted",
+        sunlight_propagates = true,
+        walkable = false,
+        light_source = 5,
+        is_ground_content = false,
+        selection_box = {
+            type = "wallmounted",
+            fixed = {-0.5, -0.5, -0.5, 0.5, -0.4, 0.5}
+        },
+        groups = {cracky=3,dig_immediate=3},
+        on_construct = function(pos)
+            local msg = desc
+            local meta = minetest.get_meta(pos)
+            --meta:set_string("text", msg)
+            meta:set_string("infotext", '"' .. msg .. '"')
+        end,
+    })
+end
+
+local pictures = {   ["acougue"]="Carne Gourmet",
+}
+
+for name,desc in pairs(pictures) do
+    minetest.register_node("cidade:"..name, {
+        description = desc,
+        drawtype = "signlike",
+        inventory_image = name..".png",
+        wield_image = name..".png",
+        visual_scale = 3.0,
+        tiles = {name..".png",},
+        use_texture_alpha = true,
+        paramtype = "light",
+        paramtype2 = "wallmounted",
+        sunlight_propagates = true,
+        walkable = false,
+        light_source = 5,
+        is_ground_content = false,
+        selection_box = {
+            type = "wallmounted",
+            fixed = {-0.5, -0.5, -0.5, 0.5, -0.4, 0.5}
+        },
+        groups = {cracky=3,dig_immediate=3},
+        on_construct = function(pos)
+            local msg = desc
+            local meta = minetest.get_meta(pos)
+            --meta:set_string("text", msg)
+            meta:set_string("infotext", '"' .. msg .. '"')
+        end,
+    })
+end
+
+local pictures = {   ["paes"]="Pao Gourmet",
+}
+
+for name,desc in pairs(pictures) do
+    minetest.register_node("cidade:"..name, {
+        description = desc,
+        drawtype = "signlike",
+        inventory_image = name..".png",
+        wield_image = name..".png",
+        visual_scale = 3.0,
+        tiles = {name..".png",},
+        use_texture_alpha = true,
+        paramtype = "light",
+        paramtype2 = "wallmounted",
+        sunlight_propagates = true,
+        walkable = false,
+        light_source = 5,
+        is_ground_content = false,
+        selection_box = {
+            type = "wallmounted",
+            fixed = {-0.5, -0.5, -0.5, 0.5, -0.4, 0.5}
+        },
+        groups = {cracky=3,dig_immediate=3},
+        on_construct = function(pos)
+            local msg = desc
+            local meta = minetest.get_meta(pos)
+            --meta:set_string("text", msg)
+            meta:set_string("infotext", '"' .. msg .. '"')
+        end,
+    })
+end
+
+local pictures = {   ["flores"]="Flores",
+}
+
+for name,desc in pairs(pictures) do
+    minetest.register_node("cidade:"..name, {
+        description = desc,
+        drawtype = "signlike",
+        inventory_image = name..".png",
+        wield_image = name..".png",
+        visual_scale = 3.0,
+        tiles = {name..".png",},
+        use_texture_alpha = true,
+        paramtype = "light",
+        paramtype2 = "wallmounted",
+        sunlight_propagates = true,
+        walkable = false,
+        light_source = 5,
+        is_ground_content = false,
+        selection_box = {
+            type = "wallmounted",
+            fixed = {-0.5, -0.5, -0.5, 0.5, -0.4, 0.5}
+        },
+        groups = {cracky=3,dig_immediate=3},
+        on_construct = function(pos)
+            local msg = desc
+            local meta = minetest.get_meta(pos)
+            --meta:set_string("text", msg)
+            meta:set_string("infotext", '"' .. msg .. '"')
+        end,
+    })
+end
